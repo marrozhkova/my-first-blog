56f2ad6617de498b60fef344d46d2248b8fa01f9
marrozhkova <mar.rozhkova@gmail.com>
2017-10-07 17:28:25 +0300

    Change CSS and debug=false

--- blog/views.py
+++ blog/views.py
@@ -7,12 +7,13 @@ from django.shortcuts import redirect
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -25,6 +26,8 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
